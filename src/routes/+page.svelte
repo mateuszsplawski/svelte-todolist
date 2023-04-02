07234d3395c71ae2f322a1ed84c79a48f306b517
mateuszsplawski <mateusz.splawski@netguru.com>
2023-04-02 16:17:21 +0200

    feat: add simple todo list

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,43 @@
+<script lang="ts">
+	import Input from './Input.svelte'
+	import { derived, writable } from 'svelte/store'
+
+	let todoName = ""
+	const todosList = writable([])
+
+	const handleChange = (newValue:string) => todoName = newValue;
+
+	const addNewTodo = () => {
+		const newTodo = {name:todoName, id: Math.random() * 1000}
+		todosList.update(todos => [...todos, newTodo])
+		todoName = ""
+	}
+
+	const todos = derived(todosList, $todosList => $todosList)
+</script>
+
 <svelte:head>
 	<title>Home</title>
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
 <section>
-	<h1>Hello world</h1>
+	<h1>Todo list</h1>
+
+	<p>New todo name: {todoName}</p>
+	<Input handleChange={handleChange} placeholder="Clean up your code" />
+
+	<button on:click={addNewTodo}>Add Todo</button>
+
+	{#if $todos.length > 0}
+	<p>List of todos:</p>
+
+	{#each $todos as todo, index(todo.id)}
+	<p>{index}: {todo.name}</p>
+	{/each}
+	{:else}
+	<p>No todos to do</p>
+	{/if}
 </section>
 
 <style>
---
+++ src/routes/Input.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    export let placeholder = "Text input"
+    export let handleChange: any
+
+    let value = ""
+
+    $: value = handleChange(value)
+</script>
+
+<input bind:value placeholder={placeholder} type="text" />
